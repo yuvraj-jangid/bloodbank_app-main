9ee4ef2cabc06da8b13a587d7b86c50a1fdb9adb
Yuvraj Jangid <yuvrajjangid.4888@gmail.com>
2023-01-23 15:30:00 +0530

    first commit

---
+++ bloodbank_app-main/lib/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContextcontext) {
+    return Scaffold();
+  }
+}
